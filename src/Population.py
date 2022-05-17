153e95b6e098000819d567883bf065004c79aff2
Lucas Vilela <lucas.vilela@quintoandar.com.br>
2022-05-17 18:30:16 -0300

    feat: Bakes mutation probability in phenotypes

--- src/Population.py
+++ src/Population.py
@@ -4,13 +4,13 @@ from Phenotype import Phenotype
 
 
 class Population:
-    def __init__(self, random: Random, populationSize: int, geneLength: int) -> None:
+    def __init__(self, random: Random, populationSize: int, geneLength: int, mutationProbability: float) -> None:
         self.random = random
         self.phenotypes = [Phenotype]
 
         for i in range(0, populationSize):
-            self.phenotypes.append(Phenotype(geneLength, self.random))
+            self.phenotypes.append(Phenotype(self.random, geneLength, mutationProbability))
 
-    def mutate(self, probability: float):
+    def mutate(self):
         for phenotype in self.phenotypes:
-            phenotype.mutate(probability)
+            phenotype.mutate()
