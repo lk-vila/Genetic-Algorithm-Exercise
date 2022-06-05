4a9bba211bc034b8676852b897bfaeff1c39c674
LeonardoMeireles <leo.meireles@usp.br>
2022-06-05 11:20:37 -0300

    fix: functions

--- src/functions/Ackley.py
+++ src/functions/Ackley.py
@@ -7,14 +7,18 @@ from . import AbstractFunction
 class Ackley(AbstractFunction.AbstractFunction):
     def calculate(self, variables: List[float], phenotype: Phenotype) -> None:
         x = variables.pop(0)
+        y = variables.pop(0)
 
         n: int = 2
         a: int = 20; b: float = 0.2; c: float = 2 * math.pi
         s1: int = 0; s2: int = 0
 
-        for i in range(1, n):
-            s1 = s1 + pow(x, 2)
-            s2 = s2 + math.cos((c * x))
+    
+        s1 = s1 + pow(x, 2)
+        s2 = s2 + math.cos((c * x))
+
+        s1 = s1 + pow(y, 2)
+        s2 = s2 + math.cos((c * y))
 
         z: float = -a * math.exp( -b * math.sqrt(1/n*s1)) - math.exp(1/n*s2) + a + math.exp(1)
         phenotype.fitness = -z
@@ -25,9 +29,17 @@ class Ackley(AbstractFunction.AbstractFunction):
         x = super().convertBinList(gene[0:20]) * 0.0001 - 40
         if x > 40:
             x = 40
+        if x < -40:
+            x = -40
+        y = super().convertBinList(gene[20:40]) * 0.0001 - 40
+        if y > 40:
+            y = 40
+        if y < -40:
+            y = -40
         variables.append(x)
+        variables.append(y)
         return variables
 
 
     def getGeneLength(self):
-        return 20
+        return 40
--- src/functions/SumS.py
+++ src/functions/SumS.py
@@ -7,21 +7,26 @@ from . import AbstractFunction
 class SumS(AbstractFunction.AbstractFunction):
     def calculate(self, variables: List[float], phenotype: Phenotype) -> None:
         x = variables.pop(0)
-        n: int = 2
-        y: float = 0
+        y = variables.pop(0)
 
-        for j in range(1, n):
-            y = y + j * pow(x, 2)
+        s: float = 0
 
-        phenotype.fitness = -y
+        s = s + 1 * pow(x, 2)
+
+        s = s + 2 * pow(y, 2)
+
+        z = s
+        phenotype.fitness = -z
 
 
     def interpretGene(self, gene: List[int]) -> List[float]:
         variables: List = []
         x = super().convertBinList(gene[0:18]) * 0.0001 - 10
+        y = super().convertBinList(gene[18:36]) * 0.0001 - 10
         variables.append(x)
+        variables.append(y)
         return variables
 
 
     def getGeneLength(self):
-        return 18
+        return 36
