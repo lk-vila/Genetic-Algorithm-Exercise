fce2aed831d137dfd72f1426f2a0565a1ea47685
LeonardoMeireles <leo.meireles@usp.br>
2022-06-05 10:30:44 -0300

    fix: gold function

--- src/Functions/Gold.py
+++ src/Functions/Gold.py
@@ -7,9 +7,9 @@ class Gold(AbstractFunction.AbstractFunction):
         x = variables.pop(0)
         y = variables.pop(0)
 
-        a: float = 1 + pow((x + y + 1), 2) * pow( (19 - 14 * x + 3 * pow(x, 2) - 14 * y + 6 * x * y + 3 * pow(y, 2) ) ,2)
+        a: float = 1 + pow((x + y + 1), 2) * (19 - 14 * x + (3 * pow(x, 2))) - 14 * y + 6 * x * y + (3 * pow(y, 2) )
         b: float = 30 + pow((2 * x - 3 * y), 2) * (18 - 32 * x + 12 * pow(x, 2) + 48 * y - 36 * x * y + 27 * pow(y, 2) )
-        return a*b
+        print(a*b)
 
     def interpretGene(self, gene: List[int]) -> List[float]:
         variables: List = []
